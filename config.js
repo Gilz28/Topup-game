22d5f4e6ac065dc631d6c30afa9f386b92e2c610
Gilzbot <agilawaludin103@gmail.com>
2025-08-04 21:12:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const API_ID = "WCTGqP3s";
-const API_KEY = "q7dppGeBIzk4ikI0b5in1rQM3uzrPYH3z2wk15LLqwSkk8nZ51RPaqA6jH5Pm1p7";
-const SIGN = "5ff8c4367f1216e47198316e93b4c21c";
+const API_ID = "isi api-id";
+const API_KEY = "isi apikey";
+const SIGN = "isi sign";
